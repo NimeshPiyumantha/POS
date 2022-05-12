e2dbb81a51657a12dc40d40cf3942b58d144b416
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2022-05-12 12:08:12 +0530

    Fully Refactored PurchaseOrderController with PurhcaseOrderBOImpl

--- Layered_Architecture/src/dao/custom/impl/QueryDAOImpl.java
+++ Layered_Architecture/src/dao/custom/impl/QueryDAOImpl.java
@@ -1,8 +1,13 @@
 package dao.custom.impl;
 
+import dao.SQLUtil;
 import dao.custom.QueryDAO;
+import model.CustomDTO;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
 
 /**
  * @author : Sanu Vithanage
@@ -11,7 +16,12 @@ import java.sql.SQLException;
 public class QueryDAOImpl implements QueryDAO {
 
     @Override
-    public void searchOrderByOrderID(String id)throws SQLException,ClassNotFoundException{
-        String sql="select Orders.oid,Orders.date,Orders.customerID,OrderDetails.itemCode,OrderDetails.qty,OrderDetails.unitPrice from Orders inner join OrderDetails on Orders.oid=OrderDetails.oid where Orders.oid=\"OID-001\";";
+    public ArrayList<CustomDTO> searchOrderByOrderID(String id) throws SQLException, ClassNotFoundException {
+        ResultSet rst = SQLUtil.executeQuery("select Orders.oid,Orders.date,Orders.customerID,OrderDetails.itemCode,OrderDetails.qty,OrderDetails.unitPrice from Orders inner join OrderDetails on Orders.oid=OrderDetails.oid where Orders.oid=?;", id);
+        ArrayList<CustomDTO> orderRecords = new ArrayList();
+        while (rst.next()) {
+            orderRecords.add(new CustomDTO(rst.getString(1), LocalDate.parse(rst.getString(2)), rst.getString(3), rst.getString(4), rst.getInt(5), rst.getBigDecimal(6)));
+        }
+        return orderRecords;
     }
 }
